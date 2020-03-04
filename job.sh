3ef944a0352fc514393d275366ce9e341e4ce52a
ldselvera <luiselvera97@gmail.com>
2020-03-04 12:01:53 -0600

    analyze and compare outputs

--- job.sh
+++ job.sh
@@ -1,27 +1,7 @@
 #! /bin/bash
-echo "comment"
-python lex_analyzer.py samples/comment.frag > out/comment.out
-echo "ident"
-python lex_analyzer.py samples/ident.frag > out/ident.out
-echo "number"
-python lex_analyzer.py samples/number.frag > out/number.out
-echo "reserve_op"
-python lex_analyzer.py samples/reserve_op.frag > out/reserve_op.out
-echo "string"
-python lex_analyzer.py samples/string.frag > out/string.out
-echo "program"
-python lex_analyzer.py samples/program.decaf > out/program.out
-echo "badbool"
-python lex_analyzer.py samples/badbool.frag > out/badbool.out
-echo "badreserve"
-python lex_analyzer.py samples/badreserve.frag > out/badreserve.out
-echo "badint"
-python lex_analyzer.py samples/badint.frag > out/badint.out
-echo "badstring"
-python lex_analyzer.py samples/badstring.frag > out/badstring.out
-echo "badop"
-python lex_analyzer.py samples/badop.frag > out/badop.out
-echo "badident"
-python lex_analyzer.py samples/badident.frag > out/badident.out
-echo "baddouble"
-python lex_analyzer.py samples/baddouble.frag > out/baddouble.out
+for i in samples/*.frag; do
+    file="$(cut -d'/' -f2 <<<"$i")"
+    filename="$(cut -d'.' -f1 <<<"$file")"
+    echo "$filename"
+    python lex_analyzer.py "$i" > out/"$filename".out || break
+done
