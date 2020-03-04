d53dca268ad0a623dd6de9b9bdd6fdda42c6d808
ldselvera <luiselvera97@gmail.com>
2020-03-04 12:05:02 -0600

    Improve script to find differences in outputs

--- diff.sh
+++ diff.sh
@@ -1,31 +1,4 @@
 #! /bin/bash
-# echo "comment"
-# diff -w out/comment.out samples/comment.out
-# echo "ident"
-# diff -w out/ident.out samples/ident.out
-# echo "number"
-# diff -w out/number.out samples/number.out
-# echo "program"
-# diff -w out/program.out samples/program.out
-# echo "reserve_op"
-# diff -w out/reserve_op.out samples/reserve_op.out
-# echo "string"
-# diff -w out/string.out samples/string.out
-# echo "badbool"
-# diff -w out/badbool.out samples/badbool.out
-# echo "badreserve"
-# diff -w out/badreserve.out samples/badreserve.out
-# echo "badint"
-# diff -w out/badint.out samples/badint.out
-# echo "badstring"
-# diff -w out/badstring.out samples/badstring.out
-# echo "badop"
-# diff -w out/badop.out samples/badop.out
-# echo "badident"
-# diff -w out/badident.out samples/badident.out
-# echo "baddouble"
-# diff -w out/baddouble.out samples/baddouble.out
-
 for i in samples/*.out; do
     file="$(cut -d'/' -f2 <<<"$i")"
     filename="$(cut -d'.' -f1 <<<"$file")"
